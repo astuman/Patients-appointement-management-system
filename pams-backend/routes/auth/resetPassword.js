ff32f6cf17b2ba57026229bd04abf5a9700e21c0
Astewul Alemu Derseh <101445231+astuman@users.noreply.github.com>
2025-01-21 23:19:05 +0200

    update code

--- pams-backend/routes/auth/resetPassword.js
+++ pams-backend/routes/auth/resetPassword.js
@@ -7,29 +7,34 @@ router.post('/', async (req, res) => {
     const uid = req.body.uid;
     const email = req.body.email;
     const password = req.body.password;
+    const u = parseInt(uid)
     // console.log(password)
     if (!(password || email || uid)) {
-        return res.status(400).json({Messaege:"missing input fields"})
+        return res.status(400).json({ Messaege: "missing input fields" })
     } else {
-        const check = await account.findOne({ email: email })
+        const check = await account.findOne({ uid: u})
             .catch((error) => { console.log(error) }
             )
         const encryptPass = await bcrypt.hash(password, 10)
         if (!(check)) {
-            return res.status(400).json({Message:"Email not found"})
+            return res.status(403).json({ Message: "user not found" })
         } else {
             const foundUi = check.uid;
-            const foundId = check._id
-            if (foundUi != uid ) {
-                return res.status(400).json({Message:"verification error"})
-            } else {
-                await account.findByIdAndUpdate(foundId)
-                    .then((newData => {
-                        newData.password = encryptPass;
-                        newData.save()
-                        return res.status(200).json({Message:"successfully changed"})
-                    }))
-            }
+            const foundId = check._id;
+            const foundEmail = check.email;
+            if (foundUi != uid) {
+                return res.status(400).json({ Message: "verification error" })
+            } else
+                if (foundEmail !== email) {
+                    return res.status(400).json({ Message: "email does not exist" })
+                } else {
+                    await account.findByIdAndUpdate(foundId)
+                        .then((newData => {
+                            newData.password = encryptPass;
+                            newData.save()
+                            return res.status(200).json({ Message: "successfully changed" })
+                        }))
+                }
         }
     }
 })
--- pams-frontend/src/components/resetPassword.js
+++ pams-frontend/src/components/resetPassword.js
@@ -12,6 +12,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 export const Resetpassword = () => {
+
   const navigate = useNavigate()
   const [uid, setUid] = useState('')
   const [email, setEmail] = useState('');
@@ -19,111 +20,141 @@ export const Resetpassword = () => {
   const [cpassword, setCpassword] = useState('')
   const [verification, setVerification] = useState('');
 
-  const handleSubmit =  () => {
+
+  const handleSubmit = () => {
     const datas = {
       uid: uid,
       email: email,
       password: password
     }
-  //  const user = api.get(`employee/find/${email}`)
-  //   if(!(user)){
-  //     alert("Invalid Email")
-  //   }else{
-    
-      if (password !== cpassword) {
-        alert("different passwords")
-      } else if (email === "" || uid === "" || password === "" || cpassword === "") {
-        alert("all fields are required")
-        navigate('#')
-      } else {
+    //  const user = api.get(`employee/find/${email}`)
+    //   if(!(user)){
+    //     alert("Invalid Email")
+    //   }else{
+    const u = parseInt(uid)
+
+    if (password !== cpassword) {
+      alert("different passwords")
+    } else if (email === "" || uid === "" || password === "" || cpassword === "") {
+      alert("all fields are required")
+      navigate('#')
+    } else {
       try {
-        api.post('/resetpassword', (datas))
-        .then(res =>{
-          alert("success")
-          navigate('/login')
-                 })
-      } catch (err) {
-        console.log(err)
-      }
-    }
+        const user = api.get(`/employee/find/${u}`)
+          .then((res) => {
+            if (user) {
+              const e = res.data.email
+              const id = res.data.uid
+              if (e !== email) {
+                alert("Invalid Email")
+              }else{
+                api.post('/resetpassword', (datas))
+                .then(res => {
+                  alert("success")
+                  navigate('/login')
+                })
+            }
+              
+            } else if (!(user)) {
+              api.get(`/employee/find/${email}`)
+                .then((res) => {
+                  const id = res.data.uid
+                  if (id !== uid) {
+                    alert("Invalid ID")
+                  } else {
+                    api.post('/resetpassword', (datas))
+                      .then(res => {
+                        alert("success")
+                        navigate('/login')
+                      })
+                  }
+                
+                })
+
+      }})
+
+  } catch (err) {
+    console.log(err)
+  }
+}
   }
 
-  return (
-    <Container component="main" maxWidth="xs" >
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          maxWidth: 550,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          border: 0.5,
-          borderRadius: 5,
-          maskPosition: 2
-        }}
-      >
-        <Typography component="h1" variant="h5" >
-          Reset password
-        </Typography>
-        <Box maxWidth={300}>
-          <TextField
-            margin="normal"
-            required="true"
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete='off'
-            type='email'
-            autoFocus
-            onChange={(e) => setEmail(e.target.value)}
+return (
+  <Container component="main" maxWidth="xs" >
+    <CssBaseline />
+    <Box
+      sx={{
+        marginTop: 8,
+        maxWidth: 550,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        border: 0.5,
+        borderRadius: 5,
+        maskPosition: 2
+      }}
+    >
+      <Typography component="h1" variant="h5" >
+        Reset password
+      </Typography>
+      <Box maxWidth={300}>
+        <TextField
+          margin="normal"
+          required="true"
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete='off'
+          type='email'
+          autoFocus
+          onChange={(e) => setEmail(e.target.value)}
 
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="uid"
-            label="Verification code"
-            name="uid"
-            autoComplete='off'
-            type='text'
-            onChange={(e) => setUid(e.target.value)}
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="off"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="cpassword"
-            label="Confirm Password"
-            type="password"
-            onChange={(e) => setCpassword(e.target.value)}
-            autoComplete="off"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            onClick={handleSubmit}
-          >
-            Reset Passowrd
-          </Button>
-        </Box>
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="uid"
+          label="Verification code"
+          name="uid"
+          autoComplete='off'
+          type='Number'
+          onChange={(e) => setUid(e.target.value)}
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="off"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="cpassword"
+          label="Confirm Password"
+          type="password"
+          onChange={(e) => setCpassword(e.target.value)}
+          autoComplete="off"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          onClick={handleSubmit}
+        >
+          Reset Passowrd
+        </Button>
       </Box>
-    </Container>
-  );
+    </Box>
+  </Container>
+);
 }
 export default Resetpassword
